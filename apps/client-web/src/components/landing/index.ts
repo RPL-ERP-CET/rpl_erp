01afeeb4f48d650ec0b190097f8dc71efc052e62
Athul Anoop <athulanoop2020@gmail.com>
2025-08-08 06:59:56 +0530

    add: convenience exports for all landing page components

---
+++ apps/client-web/src/components/landing/index.ts
@@ -0,0 +1,6 @@
+export { default as Nav } from "@client-web/components/landing/nav";
+export { default as BannerCarousel } from "@client-web/components/landing/banner-carousel";
+export { default as SearchBar } from "@client-web/components/landing/search-bar";
+export { default as ProductsTable } from "@client-web/components/landing/products-table";
+export { default as Hero } from "@client-web/components/landing/hero";
+export { default as Footer } from "@client-web/components/landing/footer";
