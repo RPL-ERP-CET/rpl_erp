818fdbb0363b0b498c4c905642c1f4591d3cee94
sharon504 <sharonpshajan@gmail.com>
2025-08-24 23:20:43 +0530

    Fixing tests configuration and test in app controller

--- apps/server/vite.config.ts
+++ apps/server/vite.config.ts
@@ -1,18 +1,19 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   plugins: [],
   test: {
+    globals: true,
     deps: {
       interopDefault: true,
     },
-    environment: 'node',
+    environment: "node",
     coverage: {
-      provider: 'v8',
-      reporter: ['text', 'html'],
+      provider: "v8",
+      reporter: ["text", "html"],
     },
-    reporters: 'default',
-    include: ['**/*.spec.ts'],
+    reporters: "default",
+    include: ["**/*.spec.ts"],
   },
-  root: '.',
+  root: ".",
 });
