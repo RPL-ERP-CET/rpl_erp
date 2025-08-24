8bd76743e598bc827d3ca21655389950448f195e
Athul Anoop <athulanoop2020@gmail.com>
2025-08-24 14:46:53 +0530

    add: mock data

---
+++ apps/client-web/src/features/landing/mocks/heroMockData.ts
@@ -0,0 +1,33 @@
+export const HERO_CAROUSEL_ITEMS = [
+  {
+    title: "Rehabilitation Plantations Ltd",
+    description:
+      "Rehabilitation Plantations Ltd, producers of natural rubber since 1976 and manufacturers of rubber based products!",
+    image:
+      "https://content.jdmagicbox.com/comp/kollam/36/9999pmulblrstd2001136/catalogue/rehabilitation-plantations-ltd-registered-office-punalur-kollam-government-organisations-mytnu5nhop.jpg",
+  },
+  {
+    title: "Quality and Environment Management",
+    description:
+      "Rated among the best in India with ISO certification for Quality and Complying with ISO standard for Environment Management System",
+    image: "https://img.gujaratijagran.com/2023/10/Rubber-one.jpg",
+  },
+];
+
+export const PRODUCTS = [
+  {
+    displayName: "60%  Latex  (in Barrels) w.e.f.25.06.2025",
+    unit: "kg",
+    rate: 10,
+  },
+  {
+    displayName: "60%  Latex (in Tankers) w.e.f.25.06.2025",
+    unit: "kg",
+    rate: 10,
+  },
+  {
+    displayName: "ISNR 10 (w.e.f. 21.02.2025)",
+    unit: "kg",
+    rate: 10,
+  },
+];
---
+++ apps/client-web/src/features/landing/mocks/index.ts
@@ -0,0 +1,4 @@
+export {
+  HERO_CAROUSEL_ITEMS,
+  PRODUCTS,
+} from "@client-web/features/landing/mocks/heroMockData";
