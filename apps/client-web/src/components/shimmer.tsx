ca2b48b40bd407eec9ad54d2e3ebc691fa982267
Athul Anoop <athulanoop2020@gmail.com>
2025-08-24 14:47:40 +0530

    add: shimmer component for skeleton rendering

---
+++ apps/client-web/src/components/shimmer.tsx
@@ -0,0 +1,6 @@
+/* Shimmer Effect */
+export default function Shimmer() {
+  return (
+    <div className="absolute inset-0 -translate-x-full [animation:shimmer_2s_infinite] bg-gradient-to-r from-transparent via-white/40 to-transparent" />
+  );
+}
