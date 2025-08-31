92e2029593f025769cac737bf59c00bd318e5f35
Athul Anoop <athulanoop2020@gmail.com>
2025-08-31 14:04:08 +0530

    docs: configure swagger documentation integration with nestjs
    automatically generates documentation based on implemented controllers
    and services

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,13 +1,25 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
-//import { RolesGuard } from "./common/guards/roles/roles.guard";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
+// import { RolesGuard } from "./common/guards/roles/roles.guard";
 
+const PORT = process.env.PORT || 4000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  //app.useGlobalGuards(new RolesGuard());
-  await app.listen(process.env.PORT ?? 4000);
+  // app.useGlobalGuards(new RolesGuard());
+
+  const config = new DocumentBuilder()
+    .setTitle("Server")
+    .setDescription("The server for RPL-ERP API")
+    .setVersion("1.0")
+    .build();
+
+  const documentFactory = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api/v1/docs", app, documentFactory);
+
+  await app.listen(PORT);
 }
 bootstrap()
   .then(() => console.log(`Server is running on ${process.env.PORT}`))
