fec8f1411beb566adb40a960294894adb0450d27
Athul Anoop <athulanoop2020@gmail.com>
2025-08-31 22:11:21 +0530

    chore: formatting

--- apps/client-web/src/services/config/api.ts
+++ apps/client-web/src/services/config/api.ts
@@ -2,32 +2,33 @@ import axios from "axios";
 import { useAuthStore } from "@client-web/store/auth";
 
 export type T_ApiSuccessResponse<T> = {
-    success: boolean;
-    data: T;
-    meta: {
-        timestamp: string;
-    };
+  success: true;
+  data: T;
+  meta: {
+    timestamp: string;
+  };
 };
 
 export type T_ApiErrorResponse = {
-    type: string;
-    title: string;
-    status: number;
-    detail: string;
-    instance: string;
+  success: false;
+  type: string;
+  title: string;
+  status: number;
+  detail: string;
+  instance: string;
 };
 
 export type T_ApiResponse<T> = T_ApiSuccessResponse<T> | T_ApiErrorResponse;
 
 const api = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL,
-    withCredentials: true,
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  withCredentials: true,
 });
 
 api.interceptors.request.use((config) => {
-    const token = useAuthStore.getState().token;
-    config.headers.Authorization = `Bearer ${token}`;
-    return config;
+  const token = useAuthStore.getState().token;
+  config.headers.Authorization = `Bearer ${token}`;
+  return config;
 });
 
 export default api;
