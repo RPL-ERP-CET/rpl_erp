99fb0aa41becdb7b4585a9f973adddc22f9f055e
Athul Anoop <athulanoop2020@gmail.com>
2025-08-31 14:04:08 +0530

    docs: configure swagger documentation integration with nestjs
    automatically generates documentation based on implemented controllers
    and services

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,8 +1,19 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle("Server")
+    .setDescription("The server for RPL-ERP API")
+    .setVersion("1.0")
+    .build();
+
+  const documentFactory = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api/v1/docs", app, documentFactory);
+
   await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap()
