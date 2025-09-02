82f3e0c1eda8a13efa311d62307e99b6d1130170
sharon504 <sharonpshajan@gmail.com>
2025-09-02 21:55:54 +0530

    feat: Added authentication module for users module

--- apps/server/src/users/users.module.ts
+++ apps/server/src/users/users.module.ts
@@ -1,11 +1,12 @@
-import { Module } from "@nestjs/common";
+import { forwardRef, Module } from "@nestjs/common";
 import { UsersController } from "./users.controller";
 import { UsersService } from "./users.service";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { User } from "./users.entity";
+import { CommonModule } from "src/common/common.module";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]), forwardRef(() => CommonModule)],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
