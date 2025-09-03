854d572e18a60e5c1e42ed0d369aa1e1d06682f3
sharon504 <sharonpshajan@gmail.com>
2025-09-03 13:02:50 +0530

    fix: Removed instances of auth guard to avoid redundancy

--- apps/server/src/users/users.controller.ts
+++ apps/server/src/users/users.controller.ts
@@ -7,36 +7,30 @@ import {
   Param,
   Body,
   ParseUUIDPipe,
-  UseGuards,
 } from "@nestjs/common";
 import { UsersService } from "./users.service";
 import { CreateUserDto } from "./dto/create-user.dto";
-import { AuthGuard } from "src/common/guards/auth/auth.guard";
 
 @Controller("users")
 export class UsersController {
   constructor(private readonly service: UsersService) {}
 
   @Get()
-  @UseGuards(AuthGuard)
   getUsers() {
     return this.service.getUsers();
   }
 
   @Get(":id")
-  @UseGuards(AuthGuard)
   getUser(@Param("id", ParseUUIDPipe) id: string) {
     return this.service.getUser(id);
   }
 
   @Post()
-  @UseGuards(AuthGuard)
   createUser(@Body() dto: CreateUserDto) {
     return this.service.createUser(dto);
   }
 
   @Put(":id")
-  @UseGuards(AuthGuard)
   updateUser(
     @Param("id", ParseUUIDPipe) id: string,
     @Body() dto: Partial<CreateUserDto>,
@@ -45,7 +39,6 @@ export class UsersController {
   }
 
   @Delete(":id")
-  @UseGuards(AuthGuard)
   deleteUser(@Param("id", ParseUUIDPipe) id: string) {
     return this.service.deleteUser(id);
   }
